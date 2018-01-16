3f80bc720fc68b47fbbd2bd51df989e775f08e37
zhiwei_zhai <zhiwei_zhai@kingdee.com>
2018-01-16 14:36:43 +0800

    add-template-ejs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const ke = require('./lib/ke')
 const path = require('path')
-const bodyParse = require('./lib/body-parser')
+//第三方解析，有json, file等各种情况，待研究。。。。。。。。。
+const bodyParser = require('body-parser')
+const urlencodeParser = bodyParser.urlencoded({extended: true})
 const app = ke();
-app.use(bodyParse)
+app.use(urlencodeParser)
 app.use(ke.static(path.join(__dirname, 'static')))
+app.set('views', path.join(__dirname, 'views'));
 app.use(function(req, res, next){
     console.log('middle ware')
     next()
@@ -15,13 +18,18 @@ app.use('/user', function (req, res, next){
 })
 
 app.use('/hello', function (req, res){
-    console.log('res hello')
+    console.log('res hello:'+req.query)
     res.send('hello')
 })
 
 app.use('/query', function (req, res){
     console.log('res query')
-    res.send('query:'+req.query)
+    res.render('query', {
+        title: 'query-html',
+        name: 'KE',
+        content: 'this is node frame KE, like express, this page is use ejs to render.'
+    })
+    // res.send('query:'+JSON.stringify(req.body))
 })
 
 app.use(function(req, res){
